bb3dc4abf792cf1df120c3e6c1d09c3bcf13cff3
Sean Phillips <seanphillips9696@gmail.com>
2021-10-03 17:07:41 +0200

    city page

---
+++ pages/location/[city].js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export async function getServerSideProps(contect) {
+  return {
+    
+  }
+}
+
+export default function City() {
+  return (
+    <div>
+      <h1>City Page</h1>
+    </div>
+  )
+}
