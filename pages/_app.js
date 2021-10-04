40df4818f2eb8b74b43458ff4dc007fff21d232c
Sean Phillips <seanphillips9696@gmail.com>
2021-10-04 19:13:35 +0200

    loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
+import React from "react";
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import "../styles/main.scss";
 
 function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    const start = () => NProgress.start();
+    const end = () => NProgress.done();
+
+    Router.events.on("routChangeStart", start);
+    Router.events.on("routChangeComplete", end);
+    Router.events.on("routChangeError", end);
+
+    return () => {
+      Router.events.off("routChangeStart", start);
+      Router.events.off("routChangeComplete", end);
+      Router.events.off("routChangeError", end);
+    }
+
+  }, [])
+
   return <Component {...pageProps} />
 }
 
